87dfe271384791e558f2c14741ddbda6019bca4a
darwin subramaniam <darwin.subramaniam@stud.hn.de>
2015-10-22 09:19:49 +0200

    simple python rule on naming type correction

--- udarduino.py
+++ udarduino.py
@@ -4,11 +4,9 @@ __author__ = 'Darwin Subramaniam'
 import serial
 import time
 
-class udarduino():
+class Udarduino:
 
-
-
-    #function to make connection to the Arduin
+    #function to make connection to the Arduino
     def connectserial(self, usbport, baud, timeout, sleeptime=int, debugmode=bool):
         try:
             arduino = serial.Serial(usbport, baud, timeout=timeout)
@@ -17,8 +15,8 @@ class udarduino():
                 print("The Arduino is connected")
         except:
             print("The Arduino could not be connect")
-            print("1.This could be due to another serialComunication is busy with Arduino")
+            print("1.This could be due to another serialCommunication is busy with Arduino")
             print("2.The Arduino is not connect to the given USB Port")
 
     #This function should deal with all the read function available in the pyserial library
-    def recievemessagesfromarduino(self,read):
+    def recieve_messages_from_arduino(self, msgtype_byte1_string2 = int , lines1_line2_certainlength3 = int, iflength= int):
