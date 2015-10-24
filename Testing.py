617a2ea13ac21783188f614f4ee676e83ed8cea4
darwin subramaniam <darwin.subramaniam@stud.hn.de>
2015-10-24 09:31:14 +0200

    added some function to reconfigure loop

--- Testing.py
+++ Testing.py
@@ -1,16 +1,22 @@
-import  serial
+import serial
 import time
 
 print("Initalizing Serial port...")
-arduino = serial.Serial('/dev/cu.usbmodem1441',9600,timeout = 1) # 2=Com3 on windows always a good idea to sepcify a timeout incase we send bad data
-time.sleep(2) #wait for initialize
-print ("Initialization Complete")
+arduino = serial.Serial('/dev/cu.usbmodem1431', 9600,
+                        timeout=1)  # 2=Com3 on windows always a good idea to sepcify a timeout incase we send bad data
+time.sleep(2)  # wait for initialize
+print("Initialization Complete")
 
 command = b'1\n'
-print(command)
-print(type(command))
 
 arduino.write(command)
 
-time.sleep(2)
-arduino.write(b'-1\n')
+time.sleep(0.2)
+
+
+
+devicemsg = arduino.readlines()
+currentCondition = devicemsg[1]
+print(type(currentCondition))
+print(str(currentCondition.decode('utf-8')))
+print(currentCondition)
--- mainserver.py
+++ mainserver.py
@@ -1,26 +1,24 @@
 __author__ = 'udlab'
 
-
 from twisted.internet.protocol import Factory, Protocol
 from twisted.internet import reactor
 import serial
 import time
 
-#Initializing the arduino Connection in mac change /dev/cu.usbmodem1441 to apprioprate
-#arduino = serial.Serial('/dev/cu.usbmodem1441', 9600)
-#time.sleep(2)
 
+#Server Initialization code
 debugmode = False
 loop = True
+device_configured = bool
+configure_now = True
+device_connected = serial.Serial()
+
 while loop:
     print("Hello Im the Tcp Client")
     print(" please initialize certian parameters before running the server")
     print(" Setting the Server in debugmode? yes = y/Y or no  = n/N")
     x = input()
     print()
-
-
-
 
     if x == 'y' or x == 'Y':
         debugmode = True
@@ -34,69 +32,98 @@ while loop:
     else:
         print("The answer is not acceptable")
 
-
 time.sleep(1)
 
-
-
-#if the connection is based in linux then uncomment this part
 print()
-print("Initalizing Serial port...")
-try:
-    arduino = serial.Serial('/dev/ttyACM0', 9600)
-    time.sleep(2)
-    print ("Arduino initialization Complete")
-    print()
-except:
-    print("The Arduino could not be connected!")
-    print("1.This could be due to another serialCommunication is busy with Arduino")
-    print("2.The Arduino is not connect to the given USB Port")
-    print()
+
+while configure_now:
+    print("Connecting Device in serial port...")
+    try:
+        # Initializing the arduino Connection in mac change /dev/cu.usbmodem1441 to apprioprate '/dev/cu.usbmodem1431'
+        device_connected = serial.Serial('/dev/cu.usbmodem1431', 9600, timeout=1) #timeout is important action
+        # time.sleep(2)
+        # arduino = serial.Serial('/dev/ttyACM0', 9600)
+        time.sleep(2)
+        print("Arduino initialization Complete")
+        device_configured = True
+        configure_now = False
+        print()
+    except:
+        print("The Arduino could not be connected!")
+        print("1.This could be due to another serialCommunication is busy with Arduino")
+        print("2.The Arduino is not connect to the given USB Port")
+        print()
+
+        #Configure Arduino later
+        print("Do you want to connect the device now?")
+        x_1 = input()
+        if x_1 == 'y' or x_1 == 'Y':
+            print("Connect the Device to the server")
+
+            print("Reconnect by pressing c ")
+            connect_now = input()
+            if connect_now == 'c' or connect_now == 'C':
+                connect_now = True
+
+        if x_1 == 'n' or x_1 == 'N':
+            device_configured = False
+            configure_now = False
 
 
-#Server Side
+print()
+
+
+
+# Server Side
 class Server(Protocol):
-        def connectionMade(self):
-            self.factory.clients.append(self)
-            print(" clients are ", self.factory.clients)
+    def connectionMade(self):
+        self.factory.clients.append(self)
+        print(" clients are ", self.factory.clients)
 
-        def connectionLost(self, reason):
-            self.factory.clients.remove(self)
+    def connectionLost(self, reason):
+        self.factory.clients.remove(self)
 
-        def dataReceived(self, data):
-            decodedata = bytes.decode(data,'utf-8')
-            a = decodedata.split(':')
+    def dataReceived(self, data):
+        decodedata = bytes.decode(data, 'utf-8')
+        a = decodedata.split(':')
 
+        if debugmode:
             print(a)
 
-            if len(a) > 1:
-                command = a[0]
-                content = a[1]
+        if len(a) > 1:
+            command = a[0]
+            content = a[1]
 
-                msg = ""
-                if command == "iam":
-                    self.name = content
-                    msg = self.name + " has joined "
-                    bytemsg = str.encode(msg)
+            msg = ""
+            if command == "iam":
+                self.name = content
+                msg = self.name + " has joined "
+                bytemsg = str.encode(msg)
 
-                elif command == "msg":
-                    msg = self.name + " : " + content
-                    bytemsg = str.encode(msg)
-                    stringCommand = content
-                    self.devicecommand(stringCommand)
+            elif command == "msg":
+                msg = self.name + " : " + content
+                bytemsg = str.encode(msg)
+                stringCommand = content
+                self.devicecommand(stringCommand)
 
-
-                for c in self.factory.clients:
-                    c.message(msg)
-
+            elif command == "configure":
+                pass
 
 
-        def message(self, message):
-            # the transmission is in byte
-            byteMessage = str.encode(message)
-            self.transport.write(byteMessage)
 
-        def devicecommand(self, message):
+            for c in self.factory.clients:
+                c.message(msg)
+
+    def message(self, message):
+        # the transmission is in byte
+        byteMessage = str.encode(message)
+        self.transport.write(byteMessage)
+
+
+
+    def devicecommand(self, message):
+        if device_configured:
+            devicemsg = deviceConnected_sent_msg()
             msg = message
             inByte = b''
 
@@ -111,37 +138,42 @@ class Server(Protocol):
             elif msg == "Green\r\n":
                 inByte = b'3\n'
             elif msg == "NGreen\r\n":
-               inByte = b'-3\n'
+                inByte = b'-3\n'
             else:
                 print("there is no command")
 
-            arduino.write(inByte)
+            device_connected.write(inByte)
             time.sleep(0.2)
-            print(inByte)
-
-class arduinomsg():
-     def device_send_msg(self):
-            seq = []
-
-            if debugmode:
-                print("funtion to read the device transmitted data is running")
-
-            for c in arduino.read():
-                seq.append(c)
-
-                if(c == '\n'):
-                    self.message(seq)
-                    if debugmode:
-                        print()
-                        print("This is the data from the device")
-                        print(seq)
-                        print()
-                    print(seq)
-                    seq = []
+            devicemsg.device_send_msg()
+            time.sleep(.2)
+        else:
+            print("The Device is not properly configured")
+            self.message("The Server is not configured properly to the server")
+            print("Do you want to reconfigure the server to the device?")
+            #connectionDevice('/dev/cu.usbmodem1431', 9600, timeout=1)
 
 
 
-devicemsg = arduinomsg()
+class deviceConnected_sent_msg():
+    def device_send_msg(self):
+
+        if debugmode:
+            print("Iam currently in the waiting for device send msg function")
+
+        devicemsg_byte = device_connected.readline()
+
+        if debugmode:
+            print("msg from device is read")
+            print("This is the msg:", devicemsg_byte)
+
+        devicemsg_string = str(devicemsg_byte.decode('utf-8'))
+        print(devicemsg_string)
+
+
+
+#def connectionDevice(name_of_the_USB,baudrate= int,timeout=int):
+    #device_connected= serial.Serial(name_of_the_USB,baudrate,time)
+
 
 factory = Factory()
 factory.clients = []
@@ -149,13 +181,8 @@ factory.protocol = Server
 reactor.listenTCP(8000, factory)
 time.sleep(0.5)
 print("Darwin :server started")
+
+if debugmode:
+    print("Device is connected", device_configured)
+
 reactor.run()
-
-
-
-
-
-
-
-
-
